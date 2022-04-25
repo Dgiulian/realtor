859503d068ed88b9a79f284d2aed12a11befcd2a
Diego Giuliani <giuliani.diego@gmail.com>
2022-04-24 22:21:25 -0300

    Fix exposed API Key

--- utils/fetchApi.ts
+++ utils/fetchApi.ts
@@ -6,7 +6,7 @@ export const fetchApi = async (url:string) => {
   const response = await axios.get(url, {
     headers: {
       'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-      'X-RapidAPI-Key': '711403b2f2mshf16d4e5eaa55308p1fac40jsnbb84ed2068f6',
+      'X-RapidAPI-Key': process.env.API_KEY || '',
     },
   });
   return response.data
